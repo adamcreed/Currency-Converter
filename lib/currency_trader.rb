5ebbfb6a43847a73e310f2cf32a92eb3636dc384
Adam Reed <kusa.xisa@gmail.com>
2017-01-17 22:15:57 -0500

    Added comparison between two currency_converters

--- lib/currency_trader.rb
+++ lib/currency_trader.rb
@@ -6,4 +6,19 @@ class CurrencyTrader
     @second_converter = second_converter
     @starting_currency = starting_currency
   end
+
+  def find_best_exchange
+    best_exchange = @first_converter.rates[@starting_currency][@starting_currency]
+    best_exchange_key = @starting_currency
+
+    @first_converter.rates[@starting_currency].each_key do |code|
+      if @first_converter.rates[@starting_currency][code] \
+          / @second_converter.rates[@starting_currency][code] > best_exchange
+
+        best_exchange_key = code
+      end
+    end
+
+    best_exchange_key
+  end
 end
--- spec/currency_trader_spec.rb
+++ spec/currency_trader_spec.rb
@@ -5,9 +5,9 @@ describe 'CurrencyTrader' do
                  'EUR' => {'USD' => 1.06971, 'EUR' => 1.0, 'JPY' => 120.782},
                  'JPY' => {'USD' => 0.00886, 'EUR' => 0.00828, 'JPY' => 1.0}}
 
-  second_rates = {'USD' => {'USD' => 1.0, 'EUR' => 0.93512, 'JPY' => 113.045},
-                  'EUR' => {'USD' => 1.06938, 'EUR' => 1.0, 'JPY' => 120.888},
-                  'JPY' => {'USD' => 0.00885, 'EUR' => 0.00827, 'JPY' => 1.0}}
+  second_rates = {'USD' => {'USD' => 1.0, 'EUR' => 0.93528, 'JPY' => 113.087},
+                  'EUR' => {'USD' => 1.06920, 'EUR' => 1.0, 'JPY' => 120.913},
+                  'JPY' => {'USD' => 0.00884, 'EUR' => 0.00827, 'JPY' => 1.0}}
 
   first_converter = CurrencyConverter.new(first_rates)
   second_converter = CurrencyConverter.new(second_rates)
@@ -17,8 +17,18 @@ describe 'CurrencyTrader' do
     it 'Creates a new CurrencyTrader object with two CurrencyConverter objects ' \
        'and a starting currency' do
 
-      expect(currency_trader.second_converter.rates['USD']['EUR']).to eq 0.93512
+      expect(currency_trader.second_converter.rates['USD']['EUR']).to eq 0.93528
       expect(currency_trader.starting_currency).to eq 'EUR'
+    end
+  end
+
+  describe '#find_best_exchange' do
+    it 'Finds the most beneficial exchange given two points in time and ' \
+        'a starting currency' do
+
+       best_exchange = currency_trader.find_best_exchange
+
+       expect(best_exchange).to eq 'USD'
     end
   end
 end
